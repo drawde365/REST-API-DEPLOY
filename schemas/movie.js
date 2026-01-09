7bc67b8027e62cc9ad49cb54d6dc59729873ed12
Edward Delgadillo Agurto <00123edu17@gmail.com>
2026-01-08 20:04:53 -0500

    Initial commit

---
+++ schemas/movie.js
@@ -0,0 +1,27 @@
+const z = require('zod')
+
+const MovieSchema = z.object({
+  title: z.string({
+    required_error: 'El título es obligatorio',
+    invalid_type_error: 'El título debe ser una cadena de texto'
+  }),
+  year: z.number().int().min(1900).max(2025),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  poster: z.url(),
+  genre: z.array(z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Romance', 'Sci-Fi', 'Crime'])).min(1),
+  rate: z.number().min(0).max(10)
+})
+
+const validateMovie = (data) => {
+  return MovieSchema.safeParse(data)
+}
+
+const validatePartialMovie = (data) => {
+  return MovieSchema.partial().safeParse(data)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
